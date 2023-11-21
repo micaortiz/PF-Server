8f4efa58edabad7b22a33182384391a002f08095
micaortiz <mica.ayelenn984@gmail.com>
2023-11-21 17:36:12 -0300

    initial config

---
+++ server/src/server.js
@@ -0,0 +1,16 @@
+/* server config */
+const express = require("express");
+const router = require("./routes");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const server = express();
+
+server.use(morgan("dev"));
+server.use(express.json());
+server.use(cors());
+
+server.use(router);
+
+
+module.exports = server;
