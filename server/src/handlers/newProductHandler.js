2ae28214c0da051ca13140f561b0b665ab19f660
José Fernando López <fernandojlb@outlook.es>
2023-11-23 19:15:44 -0600

    fix/newProduct componenet

--- server/src/handlers/newProductHandler.js
+++ server/src/handlers/newProductHandler.js
@@ -1,7 +1,7 @@
 const {Product} = require("../db")
 
-const createProduct = async (nameProd, brand, description, price, discountPercentage, priceOnSale, image, tags, stock) =>{
-    const product = await Product.create({nameProd, brand, description, price, discountPercentage, priceOnSale, image, tags, stock})
+const createProduct = async (nameProd, brand, description, price, discountPercentage, priceOnSale, image, tags, stock, CategoryId) =>{
+    const product = await Product.create({nameProd, brand, description, price, discountPercentage, priceOnSale, image, tags, stock, CategoryId})
     try {
         return product
     } catch (error) {
