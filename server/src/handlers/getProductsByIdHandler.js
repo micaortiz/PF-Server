1c0c41940114e82cb7c9c78d4381e40511edf766
Micaela Ortiz <mica.ayelenn984@gmail.com>
2023-12-04 12:15:27 -0300

    name category

--- server/src/handlers/getProductsByIdHandler.js
+++ server/src/handlers/getProductsByIdHandler.js
@@ -1,7 +1,32 @@
-const {Product} = require("../db")
+const {Product, Category} = require("../db")
 
 const getProductsByIdHandler = async(id)=>{
-    const productId = await Product.findByPk(id)
+    const product = await Product.findByPk(id, {
+        include: {
+            model: Category,
+            attributes: ["nameCat"],
+          },
+    })
+
+    let productId;
+    if(product){
+        productId={
+            id: product.id,
+            nameProd: product.nameProd,
+            brand: product.brand,
+            descripition: product.descripition,
+            price: product.price,
+            discountPercentage: product.discountPercentage,
+            priceOnSale: product.priceOnSale,
+            image: product.image,
+            active: product.active,
+            tags: product.tags,
+            stock: product.stock,
+            category: product.Category ? product.Category.nameCat : null
+
+        }
+    }
+
     return productId;
 }
 
