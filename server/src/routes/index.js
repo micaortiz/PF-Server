1e801b233925593af87b44e577f5899785663d30
Micaela Ortiz <mica.ayelenn984@gmail.com>
2023-12-05 18:21:00 -0300

    creacion de ruta para post review

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -13,11 +13,12 @@ const router = express.Router();
 const { getAllCategoriesHandlers } = require("../handlers/getAllCategoriesHandlers");
 const { getProductsById } = require("../controllers/getProductsByIdCtrl");
 const { postCategory } = require("../controllers/postCategoryCtrl");
+const {getCountryById} = require("../controllers/country/getCountryById");
 const userRouter = require("./userRouter");
 const cartRouter = require("./cartRouter");
 const paymentsRouter = require("./paymentsRouter");
 const orderRouter = require("./orderRouter");
-const {getCountryById} = require("../controllers/country/getCountryById");
+const reviewRouter = require("./reviewRouter");
 
 //Create
 router.post("/products", newProduct);
@@ -53,6 +54,9 @@ router.use('/cart', cartRouter)
 
 /* order */
 router.use('/order', orderRouter )
+
+/* reviews */
+router.use('/reviews', reviewRouter )
 
 
 module.exports = {
