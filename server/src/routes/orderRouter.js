db6934c0d3fb698d6f5de3ffd1d2212ea6f9f1ea
Micaela Ortiz <mica.ayelenn984@gmail.com>
2023-12-07 22:01:32 -0300

    feature: order by userId

--- server/src/routes/orderRouter.js
+++ server/src/routes/orderRouter.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 const orderRouter = Router();
 
-const {
-  getAllOrder,
-} = require("../controllers/orderController/getAllOrdersCtrl");
+const { getAllOrder } = require("../controllers/orderController/getAllOrdersCtrl");
+const { getOrderByUserId } = require("../controllers/orderController/getOrderByUserIdCtrl");
 
 /* Trae el Historial de Compras */
 
 orderRouter.get("/history", getAllOrder);
+// Order by UserId
+orderRouter.get("/:UserId", getOrderByUserId)
 
 module.exports = orderRouter;
