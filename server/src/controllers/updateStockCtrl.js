24607d3781d60fd30df99030c9232e41a2e8636c
Micaela Ortiz <mica.ayelenn984@gmail.com>
2023-12-12 16:47:19 -0300

    controller and handler

--- server/src/controllers/updateStockCtrl.js
+++ server/src/controllers/updateStockCtrl.js
@@ -1,53 +1,16 @@
-const { Product, Category, Review  } = require("../db");
+const { updateStockHandler } = require("../handlers/updateStockHandler");
 
 const updateStock = async (req, res) => {
   const { id, newStock } = req.body;
   try {
-    const product = await Product.findByPk(id,{
-        include: {
-            model: Category,
-            attributes: ["nameCat"]
-        },
-    });
-    const reviews = await Review.findAll({
-        where: { productId: product.id },
-      });
-    
+    const product = await updateStockHandler(id, newStock);
+
     if (!product) return res.status(404).send("Product not found");
 
     if (newStock < 0) {
       return res.status(400).send("New stock cannot be negative");
     }
-    // Actualiza el stock del producto
-    product.stock = newStock;
-    await product.save();
-
-    let productId;
-    if (product) {
-      productId = {
-        id: product.id,
-        nameProd: product.nameProd,
-        brand: product.brand,
-        description: product.description,
-        price: product.price,
-        stock: product.stock,
-        discountPercentage: product.discountPercentage,
-        priceOnSale: product.priceOnSale,
-        image: product.image,
-        active: product.active,
-        tags: product.tags,
-        createdAt: product.createdAt,
-        updatedAt: product.updatedAt,
-        deletedAt: product.deletedAt,
-        category: product.Category ? product.Category.nameCat : null,
-        reviews: reviews.map((review) => ({
-          rating: review.rating,
-          comment: review.reviewText,
-          name: review.userName
-        })),
-      };
-    }
-    return res.status(200).json(productId);
+    return res.status(200).json(product);
   } catch (error) {
     return res.status(500).json(error.message);
   }
